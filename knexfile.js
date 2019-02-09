2265a791c02a100367bf5073774eae607944c268
Denise Escobar <descobar1490@gmail.com>
2019-02-09 12:01:43 -0600

    added my dependencies and updated my config setting

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/games.db'
+    },
+    useNullAdDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/games.db'
+    },
+    useNullAdDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+  }
+  
+
+};
